8b4d05ac63239106a75a9c23e55254cd58e27c29
Defri Indra Mahardika <defrindr@gmail.com>
2025-07-09 07:20:32 +0700

    feat: enhance dashboard with product statistics and improved layout

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Item;
+use App\Models\Warehouse;
+use App\Models\ProdukRequest;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        $product = Item::active()->count();
+        $requestItem = ProdukRequest::where('status', 'request')->count();
+        $category = Category::count();
+        $warehouse = Warehouse::count();
+
+
+        $boxes = [
+            [
+                'text' => 'Total Produk',
+                'count' => $product,
+                'icon' => 'fas fa-box',
+                'color' => 'bg-info',
+                'link' => route('items.index'),
+            ],
+            [
+                'text' => 'Produk Diminta',
+                'count' => $requestItem,
+                'icon' => 'fas fa-shopping-cart',
+                'color' => 'bg-warning',
+                'link' => route('produk-request.index'),
+            ],
+            [
+                'text' => 'Kategori Produk',
+                'count' => $category,
+                'icon' => 'fas fa-tags',
+                'color' => 'bg-success',
+                'link' => route('categories.index'),
+            ],
+            [
+                'text' => 'Gudang Tersedia',
+                'count' => $warehouse,
+                'icon' => 'fas fa-warehouse',
+                'color' => 'bg-danger',
+                'link' => route('warehouses.index'),
+            ],
+        ];
+
+        return view('dashboard', compact('boxes'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,9 +11,28 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Welcome {{ auth()->user()?->name ?? "User" }}</h3>
+                    <h3 class="card-title">Welcome {{ auth()->user()?->name ?? 'User' }}</h3>
                 </div>
                 <div class="card-body">
+                    <div class="row">
+                        @foreach ($boxes as $box)
+                            <div class="col-md-3">
+                                <a href="{{ $box['link'] }}">
+
+                                    <div class="small-box bg-{{ $box['color'] }}">
+                                        <div class="inner">
+                                            <h3 style="color:black!important">{{ $box['count'] }}</h3>
+                                            <p style="color:black!important">{{ $box['text'] }}</p>
+                                        </div>
+                                        <div class="icon">
+                                            <i class="{{ $box['icon'] }}"></i>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        @endforeach
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -25,5 +44,7 @@
 @stop
 
 @section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+    <script>
+        console.log("Hi, I'm using the Laravel-AdminLTE package!");
+    </script>
 @stop
